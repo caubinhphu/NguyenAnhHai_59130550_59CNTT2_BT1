dae2124f3a768aad5190ce39ea27bee78b37885e
Hai tran do <10a3nguyenanhhai@gmail.com>
2020-04-09 09:14:47 +0700

    add QLDS class

---
+++ src/util/QLDS.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import interfaces.IQLDS;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Hai Tran Do
+ */
+public class QLDS implements IQLDS {
+      // attributes
+  private ArrayList<CaNhan> dsCaNhan = new ArrayList<>();
+
+  // methods
+  // them ca nhan
+  @Override
+  public int them(CaNhan cn) {
+    dsCaNhan.add(cn);
+    return 1;
+  }
+
+  // xoa ca nhan
+  @Override
+  public int xoa(String ten) {
+    if (dsCaNhan.removeIf(cn -> cn.hoTen == ten)) {
+      return 1;
+    }
+    return 0;
+  }
+
+  // in ds ca nhan
+  @Override
+  public void inDS() {
+    dsCaNhan.forEach(cn -> cn.hienThiTT());
+  }
+}
